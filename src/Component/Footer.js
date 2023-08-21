0ca12b398feebee14686af5cabec247e608625dd
Minal <minaldpatil99>
2023-08-21 14:20:20 +0530

    footer
    component

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -2,92 +2,116 @@ import React from "react";
 import "./Footer.css";
 const Footer = () => {
   return (
-    <div className="container">
-      <div className="row row2">
-        <div className="col-sm-3">
-          <h4>India’s #1</h4>
-          <p>Largest Auto portal</p>
+    <footer>
+      <div className="container-fluid">
+        <div className="row">
+
+          <hr />
+
+          {/* Main footer content code */}
+          <div className="col-md-4">
+            <ul type="none">
+              {/* heading */}
+              <li className="lightb">ABOUT CARDEKHO</li>
+              {/* content starting.... */}
+              <a href="#">
+                <li>About</li>
+              </a>
+              <a href="#">
+                <li>Careers With Us</li>
+              </a>
+              <a href="#">
+                <li>Terms & Conditions</li>
+              </a>
+              <a href="#">
+                <li>Privacy Policy</li>
+              </a>
+              <a href="#">
+                <li>Corporate Policies</li>
+              </a>
+              <a href="#">
+                <li>Investors</li>
+              </a>
+              <a href="#">
+                <li>FAQs</li>
+              </a>
+            </ul>
+          </div>
+          <div className="col-md-4">
+            <ul type="none">
+              <li className="lightb">CONNECT WITH US</li>
+              <a href="#">
+                <li>Feedback</li>
+              </a>
+              <a href="#">
+                <li>Contact Us</li>
+              </a>
+              <a href="#">
+                <li>Advertise with Us</li>
+              </a>
+            </ul>
+          </div>
+          <div className="col-md-4">
+            <ul type="none">
+              <li className="lightb">OTHERS</li>
+              <a href="#">
+                <li>Health Insurance</li>
+              </a>
+              <a href="#">
+                <li>Term life insurance</li>
+              </a>
+              <a href="#">
+                <li>Crack-Ed</li>
+              </a>
+              <a href="#">
+                <li>Personal loan</li>
+              </a>
+              <a href="#">
+                <li>TyreDekho</li>
+              </a>
+              <a href="#">
+                <li>BatteryDekho</li>
+              </a>
+            </ul>
+          </div>
         </div>
-        <div className="col-md-3">
-          <h4>Car Sold</h4>
-          <p>Every 4 minute</p>
-        </div>
-        <div className="col-md-3">
-          <h4>Offers</h4>
-          <p>Stay updated pay less</p>
-        </div>
-        <div className="col-md-3">
-          <h4>Compare</h4>
-          <p>Decode the right car</p>
-        </div>
-      </div>
-      <div className="row">
+        {/* Code after horizontal line */}
+
+
         <hr />
-        {/* Main footer content code */}
-        <div className="col-md-4">
-          <ul type="none">
-            <li className="lightb">ABOUT CARDEKHO</li>
-            <li>About</li>
-            <li>Careers With Us</li>
-            <li>Terms & Conditions</li>
-            <li>Privacy Policy</li>
-            <li>Corporate Policies</li>
-            <li>Investors</li>
-            <li>FAQs</li>
-          </ul>
-        </div>
-        <div className="col-md-4">
-          <ul type="none">
-            <li className="lightb">CONNECT WITH US</li>
-            <li>Feedback</li>
-            <li>Contact Us</li>
-            <li>Advertise with Us</li>
-          </ul>
-        </div>
-        <div className="col-md-4">
-          <ul type="none">
-            <li className="lightb">OTHERS</li>
-            <li>Health Insurance</li>
-            <li>Term life insurance</li>
-            <li>Crack-Ed</li>
-            <li>Personal loan</li>
-            <li>TyreDekho</li>
-            <li>BatteryDekho</li>
-            <li>Girnar Vision Fund</li>
-          </ul>
+
+
+        <div className="row">
+          <div className="col-md-6">
+            <p class="bottom">&copy; 2023 CarSelecta Pvt. Ltd.</p>
+          </div>
+
+
+          {/* icon code */}
+          <div className="col-md-6">
+            <span className="icon">
+              <strong> Connect: </strong>
+              &nbsp;
+              <a href="#" className="me-4">
+                <i className="bi bi-facebook black-icon"></i>
+              </a>
+              <a href="#" className="me-4">
+                <i className="bi bi-twitter black-icon"></i>
+              </a>
+              <a href="#" className="me-4">
+                <i className="bi bi-youtube black-icon"></i>
+              </a>
+              <a href="#" className="me-4">
+                <i className="bi bi-instagram black-icon"></i>
+              </a>
+              <a href="#" className="me-4">
+                <i className="bi bi-linkedin black-icon"></i>
+              </a>
+            </span>
+          </div>
         </div>
       </div>
-      {/* Code after horizontal line */}
-      <hr />
-      <div className="row">
-        <div className="col-md-6">
-          <footer>
-            <p>© 2023 Girnar Software Pvt. Ltd.</p>
-          </footer>
-        </div>
-        <div className="col-md-6">
-          <strong> Connect: </strong>
-          <span className="icon">
-            &nbsp;
-            <a href="#" className="me-4">
-              <i className="bi bi-facebook"></i>
-            </a>
-            <a href="#" className="me-4">
-              <i className="bi bi-twitter"></i>
-            </a>
-            <a href="#" className="me-4">
-              <i className="bi bi-youtube"></i>
-            </a>
-            <a href="#" className="me-4">
-              <i className="bi bi-instagram"></i>
-            </a>
-            <a href="#" className="me-4">
-              <i className="bi bi-linkedin"></i>
-            </a>
-          </span>
-        </div>
-      </div>
-    </div>
+    </footer>
   );
 };
 export default Footer;
